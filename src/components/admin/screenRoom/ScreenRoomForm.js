54f9131069265ba27a15ca7719c1290479dc7c14
Marek <marek@mk-home.pl>
2022-06-22 13:55:01 +0200

    walidacja tworzenia sal kinowych

--- src/components/admin/screenRoom/ScreenRoomForm.js
+++ src/components/admin/screenRoom/ScreenRoomForm.js
@@ -1,20 +1,50 @@
 import {useState} from "react";
+import {checkIfIsInteger} from "../../../utils/Helper";
+import FormErrors from "../../common/show/FormErrors";
 
 export default function ScreenRoomForm(props) {
-  const [number, setNumber] = useState(props.number);
-  const [capacity, setCapacity] = useState(props.capacity);
+  const [number, setNumber] = useState(props.number || 0);
+  const [capacity, setCapacity] = useState(props.capacity || 0);
+  const [errors, setErrors] = useState([]);
 
   function handleSubmit(e) {
     e.preventDefault();
-    props.onHandleSubmit({number, capacity});
+    if (validate()) {
+      props.onHandleSubmit({number, capacity})
+    }
+  }
+
+  function validate() {
+    const validationErrors = [];
+
+    if (number <= 0) {
+      validationErrors.push('Pole numer musi być większa niż 0 minut.')
+    }
+
+    if (checkIfIsInteger(number)) {
+      validationErrors.push('Pole numer musi być liczbą całkowitą.')
+    }
+
+    if (capacity <= 0) {
+      validationErrors.push('Pole ilość miejsc musi być większa niż 0 minut.')
+    }
+
+    if (checkIfIsInteger(capacity)) {
+      validationErrors.push('Pole ilość miejsc musi być liczbą całkowitą.')
+    }
+
+    setErrors(validationErrors);
+    return validationErrors.length === 0;
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} onChange={() => setErrors([])}>
+      <FormErrors errors={errors} />
+
       <div className="mb-3">
         <label className="form-label">Numer</label>
         <input
-          type="number"
+          type="text"
           className="form-control"
           placeholder="Numer"
           onChange={e => setNumber(e.target.value)}
@@ -23,9 +53,9 @@ export default function ScreenRoomForm(props) {
       </div>
 
       <div className="mb-3">
-        <label className="form-label">Pojemność</label>
+        <label className="form-label">Ilość miejsc</label>
         <input
-          type="number"
+          type="text"
           className="form-control"
           placeholder="Pojemność"
           onChange={e => setCapacity(e.target.value)}
@@ -33,7 +63,7 @@ export default function ScreenRoomForm(props) {
           />
       </div>
 
-      <div className="mb-3">
+      <div className="mb-3 text-end">
         <button type="submit" className="btn btn-success">Zapisz</button>
       </div>
     </form>
