b436689e79f715d3947006bafc598d372662783f
Marek <marek@mk-home.pl>
2022-06-22 11:19:32 +0200

    oznaczenie aktualnie trwajacych seansow

--- src/components/app/repertoire/DailyRepertoire.js
+++ src/components/app/repertoire/DailyRepertoire.js
@@ -4,29 +4,35 @@ import {Link} from "react-router-dom";
 export default function DailyRepertoire(props) {
   const {repertoire, date} = props;
 
+  const today = moment(date).isSame(new Date(), "day");
+
   const movies = Object.values(repertoire).map(movie => {
-    const shows = movie.shows.map((show, index) => (
-        <Link key={index} to={`/show/${show.showID}`} className={'me-2'}>
-          <span className={'badge bg-light text-dark'} style={{'fontSize': '14px'}}>{show.timeStart}</span>
-        </Link>
-      )
+    const shows = movie.shows.map((show, index) => {
+        const isCurrentlyPlaying = today && show.timeStart < moment().format('HH:mm:ss');
+        return (
+          <Link key={index} to={`/show/${show.showID}`} className={'me-2'}>
+            <span className={`badge ${isCurrentlyPlaying ? 'bg-warning' : 'bg-light text-dark'}`} style={{'fontSize': '14px'}}>{show.timeStart}</span>
+          </Link>
+        )
+      }
     )
 
     return (
       <tr key={movie.movieID}>
-        <td style={{'width': '40%'}}><Link className={'text-decoration-none fw-bold'} to={`movie/${movie.movieID}`}>{movie.movie}</Link></td>
+        <td style={{'width': '40%'}}><Link className={'text-decoration-none fw-bold'}
+                                           to={`movie/${movie.movieID}`}>{movie.movie}</Link></td>
         <td style={{'width': '15%'}}>{movie.duration} min</td>
         <td>{shows}</td>
       </tr>
     );
   })
 
-  const today = moment(date).isSame(new Date(), "day");
 
   return (
     <div className={'row mb-3'}>
       <div className={'col'}>
-        <span style={{'fontSize': '18px'}} className={`${today ? 'badge bg-info' : 'badge bg-light text-dark'}`}>{date}</span>
+        <span style={{'fontSize': '18px'}}
+              className={`${today ? 'badge bg-info' : 'badge bg-light text-dark'}`}>{date}</span>
         <table className={'table mt-1'}>
           <tbody>
           {movies}
